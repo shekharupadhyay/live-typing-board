cdb93cda8474090568f572d21c4f9fb06dcd8a3b
Shekhar Rakesh Upadhyay <Shekharupadhyay041@gmail.com>
2025-07-06 21:40:37 -0500

    Connected to live backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000');
+const socket = io('https://live-typing-board.onrender.com');
 
 function App() {
   const [screen, setScreen] = useState('home'); // 'home' or 'board'
